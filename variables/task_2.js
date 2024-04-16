9d58e09be146fc21623aab3650aac5273f064e9e
Oleg Buiko <102520756+olezhaaa137@users.noreply.github.com>
2024-04-17 01:44:05 +0300

    solved second module

---
+++ variables/task_2.js
@@ -0,0 +1,13 @@
+var xVar = "Первое объявление";
+var xVar = "Второе объявление";
+console.log(xVar); 
+
+
+let xLet = "Первое объявление";
+//let xLet = "Второе объявление"; // Identifier 'xLet' has already been declared
+console.log(xLet);
+
+
+const xConst = "Первое объявление";
+//const xConst = "Второе объявление"; // Identifier 'xConst' has already been declared
+console.log(xConst);
---
+++ variables/task_3.js
@@ -0,0 +1,7 @@
+console.log(xVar); // Выведет: undefined
+//console.log(xLet); // Cannot access 'xLet' before initialization
+//console.log(xConst); // Cannot access 'xConst' before initialization
+
+var xVar = "Переменная объявлена с использованием var";
+let xLet = "Переменная объявлена с использованием let";
+const xConst = "Переменная объявлена с использованием const";
